58816b26689bc82294d32be7abf4fe9d094a6b2a
JerryBenn <jerrydarobenn@gmail.com>
2023-04-11 15:03:53 -0400

    new fix

--- Source Code/Tetris.java
+++ Source Code/Tetris.java
@@ -271,6 +271,7 @@ public class Tetris extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
+
 	/**
 	 * Starts the game running. Initializes everything and enters the game loop.
 	 */
@@ -585,49 +586,6 @@ public class Tetris extends JFrame {
 		Tetris tetris = new Tetris();
 		tetris.startGame();
 	}
+}
 
 	//music
-	public class Main {
-
-		public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-
-			Scanner scanner = new Scanner(System.in);
-
-			File file = new File("bgMusic.wav");
-			AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-			Clip clip = AudioSystem.getClip();
-			clip.open(audioStream);
-
-			String response = "";
-
-
-			while (!response.equals("Q")) {
-				System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
-				System.out.print("Enter your choice: ");
-
-				response = scanner.next();
-				response = response.toUpperCase();
-
-				switch (response) {
-					case ("P"):
-						clip.start();
-						break;
-					case ("S"):
-						clip.stop();
-						break;
-					case ("R"):
-						clip.setMicrosecondPosition(0);
-						break;
-					case ("Q"):
-						clip.close();
-						break;
-					default:
-						System.out.println("Not a valid response");
-				}
-
-			}
-			System.out.println("Byeeee!");
-		}
-
-	}
-}
